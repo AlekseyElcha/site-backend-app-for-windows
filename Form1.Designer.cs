2514e2bfd493bf1a269089f5a1b2b6282d44859f
alesh <aleshus2007@gmail.com>
2026-03-27 15:37:32 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace site_backend_admin
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(363, 419);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(250, 63);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Синхронизировать данные";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(363, 82);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(429, 303);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(35, 82);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(292, 756);
+            this.listBox1.TabIndex = 2;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(64, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(231, 29);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Список обращений";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(426, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(202, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Текст выбранного обращения";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(94, 136);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Войти";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(1538, 39);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(146, 51);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Войти в систему";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(34, 64);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 16);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Email:";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(84, 58);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(203, 22);
+            this.textBox1.TabIndex = 8;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(37, 110);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(211, 30);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "Запросить код";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(37, 82);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(211, 22);
+            this.textBox2.TabIndex = 10;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button4);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Location = new System.Drawing.Point(1454, 110);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(305, 260);
+            this.groupBox1.TabIndex = 11;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.textBox2);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Location = new System.Drawing.Point(1454, 401);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(305, 187);
+            this.groupBox2.TabIndex = 12;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "groupBox2";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(1055, 22);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(123, 16);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Текущий аккаунт:";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(1207, 22);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 16);
+            this.label5.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1782, 953);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,277 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static site_backend_admin.Form1;
+
+namespace site_backend_admin
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("kernel32.dll")]
+        static extern bool AllocConsole();
+
+        [DllImport("kernel32.dll")]
+        static extern bool FreeConsole();
+
+        private static readonly HttpClient client = new HttpClient();
+
+        private List<Question> allQuestions = new List<Question>();
+
+        private Question selectedQuestion;
+        private string adminEmail;
+        private string authCode;
+        public class Question
+        {
+            public string date { get; set; }
+            public string time { get; set; }
+            public string surname { get; set; }
+            public string address { get; set; }
+            public string status { get; set; }
+            public List<string> files { get; set; }
+            public string id { get; set; }
+            public string name { get; set; }
+            public string email { get; set; }
+            public string message { get; set; }
+        }
+
+        public static class ApiClient
+        {
+            private static readonly CookieContainer _cookieContainer = new CookieContainer();
+
+            public static readonly HttpClient Client = new HttpClient(
+                new HttpClientHandler
+                {
+                    CookieContainer = _cookieContainer,
+                    UseCookies = true,  // автоматически сохраняет и отправляет cookies
+                    ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => true,
+                }
+            )
+            {
+                BaseAddress = new Uri("http://localhost:8000/")
+            };
+            public static async Task<HttpResponseMessage> SendWithRefreshAsync(Func<Task<HttpResponseMessage>> request)
+            {
+                var response = await request();
+
+                if (response.StatusCode == HttpStatusCode.Unauthorized)
+                {
+                    // пробуем рефрешнуть
+                    bool refreshed = await AuthService.RefreshAsync();
+
+                    if (refreshed)
+                    {
+                        // повторяем запрос с новыми cookies
+                        response = await request();
+                    }
+                    else
+                    {
+                        // рефреш тоже не помог — разлогиниваем
+                        MessageBox.Show("Ошшибка с авторизацией - перезапустите приложение");
+                    }
+                }
+
+                return response;
+            }
+        }
+
+        public static class AuthService
+        {
+            public static async Task<bool> LoginAsync(string username, string code)
+            {
+                var payload = new { email = username, auth_code = code };
+                var json = JsonConvert.SerializeObject(payload);
+                var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+                var response = await ApiClient.Client.PostAsync("/auth/login", content);
+
+                var responseBody = await response.Content.ReadAsStringAsync();
+                MessageBox.Show($"Status: {response.StatusCode}\nBody: {responseBody}");
+                if (response.IsSuccessStatusCode)
+                {
+                    // cookies (access_token, refresh_token) сохранятся автоматически
+                    // в CookieContainer и будут отправляться при следующих запросах
+                    return true;
+                }
+
+                return false;
+            }
+            public static async Task<bool> RefreshAsync()
+            {
+                var response = await ApiClient.Client.PostAsync("auth/refresh", null);
+                return response.IsSuccessStatusCode;
+                // refresh_token cookie уйдёт автоматически, новые cookies сохранятся
+            }
+        }
+
+        public async Task<List<Question>> GetQuestionsAsync()
+        {
+            try
+            {
+                string url = "http://localhost:8000/handle_questions/all_questions";
+                HttpResponseMessage response = await client.GetAsync(url);
+
+                response.EnsureSuccessStatusCode();
+
+                string jsonResponse = await response.Content.ReadAsStringAsync();
+
+                allQuestions = JsonConvert.DeserializeObject<List<Question>>(jsonResponse);
+               
+                return allQuestions;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"{ex}");
+                throw;
+            }
+        }
+
+        public async void SendAuthCode(string email)
+        {
+            try
+            {
+                string url = "http://localhost:8000/auth/get_auth_code";
+                var formData = new List<KeyValuePair<string, string>>
+                {
+                    new KeyValuePair<string, string>("email", email)
+                };
+
+                var content = new FormUrlEncodedContent(formData);
+
+                HttpResponseMessage response = await client.PostAsync(url, content);
+
+                response.EnsureSuccessStatusCode();
+
+                string responseData = await response.Content.ReadAsStringAsync();
+                return;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"{ex}");
+                throw;
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            listBox1.DrawMode = DrawMode.OwnerDrawFixed;
+            listBox1.ItemHeight = 70;
+            listBox1.DrawItem += ListBox1_DrawItem;
+            groupBox1.Hide();
+            groupBox2.Hide();
+            label4.Hide();
+            AllocConsole();
+        }
+
+        private void ListBox1_DrawItem(object sender, DrawItemEventArgs e)
+        {
+            if (e.Index < 0) return;
+
+            e.DrawBackground();
+
+            string text = listBox1.Items[e.Index].ToString();
+
+            using (var brush = new SolidBrush(e.ForeColor))
+            {
+                e.Graphics.DrawString(text, e.Font, brush, e.Bounds);
+            }
+
+            e.DrawFocusRectangle();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            richTextBox1.Clear();
+            richTextBox1.Text = "Загрузка данных...";
+            List<Question> questions = await GetQuestionsAsync();
+            for (int i = 0; i < questions.Count; i++)
+            {
+                var q = questions[i];
+                listBox1.Items.Add($"Имя: {q.name}\nФамилия: {q.surname}\nАдрес: {q.address}\nEmail: {q.email}");
+            }
+            richTextBox1.Clear();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            richTextBox1.Text = listBox1.Items[listBox1.SelectedIndex].ToString();
+            Question selectedQuestion = allQuestions[listBox1.SelectedIndex];
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            groupBox1.Show();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            adminEmail = textBox1.Text;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (adminEmail != null)
+            {
+                SendAuthCode(adminEmail);
+                groupBox2.Show();
+            }
+        }
+
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            bool success = await AuthService.LoginAsync(adminEmail, authCode);
+            if (success)
+            {
+                MessageBox.Show("Успешная авторизация");
+                groupBox1.Hide();
+                groupBox2.Hide();
+                button3.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Ошибка авторизации");
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            authCode = textBox2.Text.ToString();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
